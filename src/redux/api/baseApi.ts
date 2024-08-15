062a9edf148fd93b42f3d7f41285f5d0da2fe7e7
saifulaija <saifulaija2023@gmail.com>
2024-08-15 13:07:17 +0600

    redux setup done

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,15 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+
+import { axiosBaseQuery } from "@/helpers/axiosBaseQuery";
+import { tagTypeList } from "./tag-types";
+
+
+// Define a service using a base URL and expected endpoints
+export const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: axiosBaseQuery({
+    baseUrl: " http://localhost:5000/api/v1",
+  }),
+  endpoints: () => ({}),
+  tagTypes: tagTypeList,
+});
---
+++ src/redux/api/tag-types.ts
@@ -0,0 +1,17 @@
+export enum tagTypes {
+  user = "user",
+  admin = "admin",
+
+  category = "category",
+  booking = "booking",
+  image = "image",
+}
+
+export const tagTypeList = [
+  tagTypes.user,
+  tagTypes.admin,
+  tagTypes.category,
+
+  tagTypes.booking,
+  tagTypes.image,
+];
